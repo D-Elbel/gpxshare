36478153b03031526b3d0a0c052621ad50bd1749
Darragh Elbel <elbeldarragh@gmail.com>
2025-09-13 15:23:30 +0100

    Adding error handling to getGeoJSON

--- app/Http/Controllers/GeoJSONController.php
+++ app/Http/Controllers/GeoJSONController.php
@@ -12,16 +12,16 @@ class GeoJSONController extends Controller
     public function saveToS3(Request $request)
     {
         try {
-            $geojson = $request->input('geojson');
+            $geoJSON = $request->input('geojson');
 
-            if (!$geojson) {
+            if (!$geoJSON) {
                 return response()->json([
                     "error" => "Missing geojson payload"
                 ], 400);
             }
 
             $uuid = Str::uuid()->toString();
-            Storage::disk('s3')->put("files/$uuid.json", json_encode($geojson));
+            Storage::disk('s3')->put("files/$uuid.json", json_encode($geoJSON));
 
             return response()->json(["uuid" => $uuid], 200);
         } catch (\Exception $e) {
@@ -34,8 +34,24 @@ class GeoJSONController extends Controller
 
     public function getGeoJSON(Request $request)
     {
-        $uuid = $request->route("uuid");
-        $geoJSON = Storage::disk('s3')->get("files/$uuid.json");
+        try {
+            $uuid = $request->route("uuid");
+            $geoJSON = Storage::disk('s3')->get("files/$uuid.json");
+
+            if (!$geoJSON) {
+                return response()->json([
+                    "error" => "Not found"
+                ], 400);
+            }
+
+        } catch(\Exception $e) {
+            Log::error("GeoJSON fetch failed: " . $e->getMessage(), ["exception" => $e]);
+            return response()->json([
+                "error" => "Failed to fetch GeoJSON"
+            ], 500);
+        }
+
+
         return response($geoJSON, 200);
     }
 }
